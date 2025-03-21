1f58df5364c78d1b2090fcb5fc7e4b4a06c0a6f2
Farid Sabirjanow <subfarid783@gmail.com>
2025-03-21 13:46:38 +0300

    create classes movie,movieresponse,poster,rating/

---
+++ Rating.java
@@ -0,0 +1,14 @@
+package com.example.mycourse;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Rating {
+    @SerializedName("kp")
+    private String kp;
+    public Rating(String kp) {
+        this.kp = kp;
+    }
+    public String getKp() {
+        return kp;
+    }
+}
